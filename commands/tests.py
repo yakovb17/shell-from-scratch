4973657b3bea9749e86d2d03d8be4b6292798bed
Yakov B <yakov10009@gmail.com>
2026-05-29 08:48:17 +0300

    Add pytest as a dependency and create initial test cases for command handling

---
+++ commands/tests.py
@@ -0,0 +1,28 @@
+from commands import EchoCommand, ExitCommand, TypeCommand, COMMANDS_NAMES_TO_CLASS
+import pytest
+
+
+def test_echo_command(capsys) -> None:
+    command_args = "test echo"
+    EchoCommand().execute([command_args])
+    assert capsys.readouterr().out.strip() == command_args
+
+
+def test_exit_command() -> None:
+    with pytest.raises(SystemExit):
+        ExitCommand().execute([])
+
+
+@pytest.mark.parametrize(
+    ("command_name", "expected_output"),
+    [
+        ("echo", "echo is a shell builtin"),
+        ("nonexistent_command", "nonexistent_command: not found"),
+        ("ls", f"ls is {TypeCommand.find_command_executable_file('ls')}"),
+    ],
+)
+def test_type_command_with_builtin_command(
+    command_name, expected_output, capsys
+) -> None:
+    TypeCommand(COMMANDS_NAMES_TO_CLASS).execute([command_name])
+    assert capsys.readouterr().out.strip() == expected_output
